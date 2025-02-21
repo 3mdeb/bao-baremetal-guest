45f5707d02a566611c183c923368260f3d25226a
Daniel Oliveira <drawnpoetry@gmail.com>
2025-02-21 15:49:12 +0000

    wip: start porting to armv8m
    Signed-off-by: Daniel Oliveira <drawnpoetry@gmail.com>

---
+++ src/arch/armv8-m/arch.mk
@@ -0,0 +1,9 @@
+CROSS_COMPILE ?= arm-none-eabi-
+
+
+ARM_PROFILE?=armv8-m.main
+ARCH_GENERIC_FLAGS +=-march=$(ARM_PROFILE) $(ARCH_SUB_GENERIC_FLAGS)
+ARCH_ASFLAGS = 
+ARCH_CPPFLAGS =	
+ARCH_LDFLAGS = 
+#ARCH_SUB_GENERIC_FLAGS:=-mfpu=fpv5-sp-d16 -mfloat-abi=hard
---
+++ src/arch/armv8-m/sources.mk
@@ -0,0 +1,2 @@
+arch_c_srcs:= init.c psci.c irq.c timer.c nvic.c
+arch_s_srcs+= exceptions.S start.S
